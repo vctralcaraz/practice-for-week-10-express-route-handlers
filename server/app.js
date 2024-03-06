6cac0cd09202839a8798bc8a9329ea7f587bd923
Victor Alcaraz <vctralcaraz@gmail.com>
2024-03-06 00:18:34 -0800

    done

--- server/app.js
+++ server/app.js
@@ -25,6 +25,28 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Body:', req.body);
+  next();
+});
+
+app.get('/artists', (req, res) => {
+  const artists = getAllArtists();
+
+  res.status(200);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(artists);
+});
+
+app.post('/artists', (req, res) => {
+  const newArtist = addArtist(req.body);
+
+  res.status(201);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(newArtist);
+});
 
 // DO NOT MODIFY
 if (require.main === module) {
